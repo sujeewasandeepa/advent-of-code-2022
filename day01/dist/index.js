4d72be7c07dd5bdd6caea04679915012251fcd4d
Sujeewa Sandeepa Kularathna <sujeewasandeepa@proton.me>
2023-06-09 11:46:13 +0530

    part one done

--- day01/dist/index.js
+++ day01/dist/index.js
@@ -1,9 +1,20 @@
 import * as fs from 'fs';
 const inputs = fs.readFileSync('inputs.txt', 'utf-8');
 const words = inputs.split("\n");
-words.map((word) => {
-    let maxTotal = 0;
-    if (word != "") {
-        console.log(word);
+let maxTotal = 0;
+let i = 0;
+while (true) {
+    let total = 0;
+    while (words[i] != '') {
+        total += parseInt(words[i]);
+        i++;
     }
-});
+    if (total > maxTotal) {
+        maxTotal = total;
+    }
+    if (i == words.length - 1) {
+        break;
+    }
+    i++;
+}
+console.log(maxTotal);
--- day01/src/index.ts
+++ day01/src/index.ts
@@ -3,9 +3,22 @@ import * as fs from 'fs';
 const inputs = fs.readFileSync('inputs.txt', 'utf-8');
 const words = inputs.split("\n");
 
-words.map((word) => {
-    let maxTotal = 0;
-    if (word != "") {
-        console.log(word); 
+let maxTotal:number = 0;
+let i:number = 0;
+
+while (true) {
+    let total:number = 0;
+    while (words[i] != '') {
+        total += parseInt(words[i]);
+        i++;
     }
-});
+    if (total > maxTotal) {
+        maxTotal = total;
+    }
+    if (i == words.length -1) {
+        break;
+    }
+    i++;
+}
+
+console.log(maxTotal);
