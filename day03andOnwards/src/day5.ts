34cd2cbebb51a69f3bdbfd7375e5ea3582cb2e8c
Sujeewa Sandeepa Kularathna <sujeewasandeepa@protonmail.ch>
2023-06-15 19:43:50 +0530

    setup for day 05

---
+++ day03andOnwards/src/day5.ts
@@ -0,0 +1,3 @@
+export const DayFive = () => {
+    console.log("day five!");
+} 
--- day03andOnwards/src/index.ts
+++ day03andOnwards/src/index.ts
@@ -1,5 +1,6 @@
 import { DayThree } from "./day3";
 import { DayFour } from "./day4";
+import { DayFive } from "./day05";
 
 console.log("--| ADVENT OF CODE 2022 |--");
 console.log("");
@@ -10,4 +11,8 @@ console.log("");
 
 console.log("--( day 04)--");
 DayFour();
+console.log("");
+
+console.log("--( day 05 )--");
+DayFive();
 console.log("");
