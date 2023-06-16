e5f5911dd1c6e54e0227847436a07621a109a16b
Sujeewa Sandeepa Kularathna <sujeewasandeepa@protonmail.ch>
2023-06-16 12:05:28 +0530

    extract the values for stack

--- day03andOnwards/src/day5.ts
+++ day03andOnwards/src/day5.ts
@@ -6,7 +6,8 @@ import { Stack } from './data-structures/Stack';
 export const DayFive = () => {
     const inputs = fs.readFileSync('./inputs/day5.txt', 'utf-8');
     let inputsSections: String[] = inputs.split("\n\n");
-    const stackStrings = inputsSections[0];
+    const stackStrings = inputsSections[0].split("\n");
+    console.log(stackStrings);
     const actions = inputsSections[1];
 
     const stack01 = new Stack<string>();
@@ -19,10 +20,20 @@ export const DayFive = () => {
     const stack08 = new Stack<string>();
     const stack09 = new Stack<string>();
 
+    const stackArray = [
+        stack01, stack02, stack03, stack03, stack04, stack05,
+        stack06, stack07, stack08, stack09
+    ]
+
     for (let row=0; row<stackStrings.length; row++) {
-        for (let col=0; col<10; col++) {
-            console.log(stackStrings[row][col]);
+        for (let col=0; col<stackStrings[row].length; col++) {
+            let character = stackStrings[row][col];
+            if (character && character != '[' && character != ']' && character != ' ') {
+                console.log(character);
+            }
         }
     }
+        
+    console.log(stack01);
     
 } 
